1c8d089510725407035db227002d423a2644bd25
Đào Ngọc Thạnh <33601978+ngocthanh06@users.noreply.github.com>
2019-04-26 11:59:29 +0700

    aaaa

---
+++ application/logs/log-2019-04-26.php
@@ -0,0 +1,102 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2019-04-26 12:45:49 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:45:49 --> No URI present. Default controller set.
+DEBUG - 2019-04-26 12:45:49 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:45:49 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:45:49 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:45:49 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/public/public_config.php
+DEBUG - 2019-04-26 12:45:49 --> Total execution time: 0.4031
+DEBUG - 2019-04-26 12:45:50 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:45:50 --> No URI present. Default controller set.
+DEBUG - 2019-04-26 12:45:50 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:45:50 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:45:50 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:45:50 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/public/public_config.php
+DEBUG - 2019-04-26 12:45:50 --> Total execution time: 0.4615
+DEBUG - 2019-04-26 12:46:03 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:46:03 --> No URI present. Default controller set.
+DEBUG - 2019-04-26 12:46:03 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:46:03 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:46:03 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:46:03 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:46:03 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-04-26 12:46:04 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:46:04 --> No URI present. Default controller set.
+DEBUG - 2019-04-26 12:46:04 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:46:04 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:46:04 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:46:04 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:46:04 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-04-26 12:46:16 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:46:16 --> No URI present. Default controller set.
+DEBUG - 2019-04-26 12:46:16 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:46:16 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:46:16 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:46:16 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/public/public_config.php
+DEBUG - 2019-04-26 12:46:16 --> Total execution time: 0.5325
+DEBUG - 2019-04-26 12:46:39 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:46:39 --> No URI present. Default controller set.
+DEBUG - 2019-04-26 12:46:39 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:46:39 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:46:39 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:46:39 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/public/public_config.php
+DEBUG - 2019-04-26 12:46:39 --> Total execution time: 0.2896
+DEBUG - 2019-04-26 12:46:46 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:46:46 --> No URI present. Default controller set.
+DEBUG - 2019-04-26 12:46:46 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:46:46 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:46:47 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:46:47 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:46:47 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-04-26 12:46:48 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:46:48 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:46:48 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:46:49 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:46:49 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:46:49 --> Session class already loaded. Second attempt ignored.
+ERROR - 2019-04-26 12:46:49 --> Could not find the language line "login_heading"
+ERROR - 2019-04-26 12:46:49 --> Could not find the language line "login_identity_label"
+ERROR - 2019-04-26 12:46:49 --> Could not find the language line "login_password_label"
+DEBUG - 2019-04-26 12:46:49 --> Total execution time: 0.6137
+DEBUG - 2019-04-26 12:47:11 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:47:11 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:47:11 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:47:11 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:47:11 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:47:11 --> Session class already loaded. Second attempt ignored.
+ERROR - 2019-04-26 12:47:11 --> Could not find the language line "login_heading"
+ERROR - 2019-04-26 12:47:12 --> Could not find the language line "login_identity_label"
+ERROR - 2019-04-26 12:47:12 --> Could not find the language line "login_password_label"
+DEBUG - 2019-04-26 12:47:13 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:47:14 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:47:14 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:47:14 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:47:14 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:47:14 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-04-26 12:47:14 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/admin/admin_config.php
+DEBUG - 2019-04-26 12:47:14 --> Total execution time: 0.3934
+DEBUG - 2019-04-26 12:53:46 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:53:46 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:53:46 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:53:46 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:53:46 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:53:46 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-04-26 12:53:46 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/admin/admin_config.php
+DEBUG - 2019-04-26 12:53:46 --> Total execution time: 0.4530
+DEBUG - 2019-04-26 12:53:51 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:53:51 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:53:51 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:53:51 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:53:51 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:53:51 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-04-26 12:53:51 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/admin/admin_config.php
+DEBUG - 2019-04-26 12:53:52 --> Total execution time: 0.4159
+DEBUG - 2019-04-26 12:53:53 --> UTF-8 Support Enabled
+DEBUG - 2019-04-26 12:53:53 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-26 12:53:53 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/common/config.php
+DEBUG - 2019-04-26 12:53:54 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+DEBUG - 2019-04-26 12:53:54 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/ion_auth.php
+DEBUG - 2019-04-26 12:53:54 --> Session class already loaded. Second attempt ignored.
+DEBUG - 2019-04-26 12:53:54 --> Config file loaded: E:\xampp\htdocs\hotel\application\config/admin/admin_config.php
+DEBUG - 2019-04-26 12:53:54 --> Total execution time: 0.7884
